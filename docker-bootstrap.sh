a86d8fada45efaef8d7e90f4da823e1de8051077
avianage <aakash.joshi.cs@gmail.com>
2026-03-02 22:15:23 +0530

    Fixed prisma's issue

--- docker-bootstrap.sh
+++ docker-bootstrap.sh
@@ -5,7 +5,8 @@ set -e
 
 echo "Running Prisma migrations..."
 # In production, we use 'migrate deploy' to apply pending migrations
-npx prisma migrate deploy
+# Use the explicitly installed version from node_modules
+./node_modules/.bin/prisma migrate deploy
 
 echo "Starting the application..."
 exec node server.js
